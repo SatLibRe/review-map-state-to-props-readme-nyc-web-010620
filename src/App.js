df5dc249158ad4c2be154b558e88b99d05da2545
Miles Higbie <miles.higbie@gmail.com>
2020-03-28 16:58:49 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ class App extends Component {
   }
 
   render() {
-    // debugger;
+    debugger;
     return (
       <div className="App">
           <button onClick={() => this.handleOnClickItems()}>
@@ -33,9 +33,15 @@ class App extends Component {
   }
 }
 
-const mapStateToProps = (state) => {
-  debugger;
-  return { items: state.items }
-}
+// const mapStateToProps = (state) => {
+//   debugger;
+//   return { items: state.items }
+// }
 
+// export default connect(mapStateToProps)(App);
+
+const mapStateToProps = (state) => {
+  return { orangePeel: ["dis", "is", 'HARD']}
+}
+ 
 export default connect(mapStateToProps)(App);
